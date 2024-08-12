fba5e267527c6317ada51b42928213914626e315
Juan Parra <parrajuan1025@gmail.com>
2024-08-12 06:33:08 -0400

    connecting to render

--- config/connection.js
+++ config/connection.js
@@ -5,10 +5,11 @@ const Sequelize = require('sequelize');
 const sequelize = process.env.DB_URL
   ? new Sequelize(process.env.DB_URL)
   : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
-      host: 'localhost',
-      dialect: 'postgres',
+      host: 'dpg-cqsu7raj1k6c73fo8b5g-a.ohio-postgres.render.com',
+      dialect: 'postgresql',
       dialectOptions: {
         decimalNumbers: true,
+        ssl: true
       },
     });
 
