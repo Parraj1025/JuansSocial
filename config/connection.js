42c56ebaf92711882c6fc955ba44b672d75a3f8e
Juan Parra <parrajuan1025@gmail.com>
2024-11-04 13:23:41 -0500

    updating environment variables

--- config/connection.js
+++ config/connection.js
@@ -5,7 +5,7 @@ const Sequelize = require('sequelize');
 const sequelize = process.env.DB_URL
   ? new Sequelize(process.env.DB_URL)
   : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
-      host: 'dpg-cqsu7raj1k6c73fo8b5g-a.ohio-postgres.render.com',
+      host: 'dpg-cskgu92j1k6c73c3i2i0-a.ohio-postgres.render.com',
       dialect: 'postgresql',
       dialectOptions: {
         decimalNumbers: true,
